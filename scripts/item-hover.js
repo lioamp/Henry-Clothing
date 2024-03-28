7aa07ebbf927c4098b1d2c578f20b6e538c697cb
Carlos Amparo <109323348+lioamp@users.noreply.github.com>
2024-03-28 22:34:29 +0800

    Delete scripts directory

--- scripts/item-hover.js
+++
@@ -1,7 +0,0 @@
-$(document).ready(function () {
-    $(".hoverable").hover(function () {
-        $(this).css("box-shadow", "0px 0px 20px 15px rgba(255, 255, 255, 0.2)");
-    }, function () {
-        $(this).css("box-shadow", "none");
-    });
-}); 
